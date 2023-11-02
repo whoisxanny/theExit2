4e6a3b715e88a97271cd80d159af3fd98c9c2ce0
whoisxanny <tuffx4nny@gmail.com>
2023-11-02 17:31:26 +0500

    initials

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+        System.out.println(Arrays.toString(array));
+        int[] newArray = noOdds(array);
+        System.out.println(Arrays.toString(newArray));
+    }
 
+
+    public static int[] noOdds(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                arr[i] += 1;
+            }
+        }
+
+        return Arrays.copyOfRange(arr, 2, 7);
     }
 }
